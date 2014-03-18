821232791f0ed7ef2ddd4e8018ce51e0684d8b2d
yourgamesbeover <yourgamesbeover@gmail.com>
2014-03-18 18:49:20 -0400

    cleanup

--- Assets/Scripts/TestEngineController.cs
+++ Assets/Scripts/TestEngineController.cs
@@ -26,15 +26,6 @@ public class TestEngineController : MonoBehaviour {
 		leftEngine.AddForce (leftEngine.transform.forward *20* leftTrigger);
 		rightEngine.AddForce(rightEngine.transform.forward*20*rightTrigger);
 
-
-//		float leftAdjust = (Input.GetAxis("LeftTrigger")-0.5f)*2;
-//		float rightAdjust = (Input.GetAxis("RightTrigger")-0.5f)*2;
-		//values are now mapped between -1.0 and 1.0
-//		float combined = Input.GetAxis("LeftTrigger")-Input.GetAxis("RightTrigger");
-
-//		leftEngine.AddRelativeTorque(new Vector3(0f,Input.GetAxis("LeftTrigger"),0f));
-//		rightEngine.AddRelativeTorque(new Vector3(0f,-Input.GetAxis("RightTrigger"),0f));
-
 		leftEngine.AddRelativeTorque(Input.GetAxis("LeftStickVert")*50,0,-Input.GetAxis("LeftStickHoriz")*50);
 		rightEngine.AddRelativeTorque(Input.GetAxis("RightStickVert")*50,0,-Input.GetAxis("RightStickHoriz")*50);
 	}

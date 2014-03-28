e16b1889f130d991cbb60f719690bcfd0b371c10
yourgamesbeover <yourgamesbeover@gmail.com>
2014-03-28 16:58:15 -0400

    revamped the skin system to use index values instead of strings; hold F2 and hit a number to change skins OTF

--- Assets/Scripts/PodSkin.cs
+++ Assets/Scripts/PodSkin.cs
@@ -5,12 +5,24 @@ public class PodSkin : MonoBehaviour {
 	public Transform basePod,baseLeft,baseRight;
 	private GameObject curPod,curLeft,curRight;
 	public bool ChangeOnStart;
-	public string initialSkin;
+	public int initialSkinIndex = 0;
 
 	void Start(){
-		if(ChangeOnStart)ChangeSkin(initialSkin);
+		if(ChangeOnStart)ChangeSkin(initialSkinIndex);
 	}
 
+	void Update(){
+		if(Input.GetKey(KeyCode.F2)){
+			for(int i = 0; i<SkinManager.SkinList.Length;i++){
+				if(Input.GetKeyDown((i).ToString())){
+					ChangeSkin(i);
+				}
+			}
+		}
+	}
+	public void ChangeSkin(int index){
+		ChangeSkin(SkinManager.SkinList[index]);
+	}
 	public void ChangeSkin(string skin){
 		Deskin();
 		Debug.Log("loading skin "+skin);
@@ -22,19 +34,19 @@ public class PodSkin : MonoBehaviour {
 			curPod = Instantiate(newPod,basePod.position,basePod.rotation)as GameObject;
 			basePod.renderer.enabled = false;
 			curPod.transform.parent = basePod;
-		}
+		}else Debug.Log ("Pod not found, reverting to default");
 		if(newLeft!=null){
 			Debug.Log("Left found!");
 			curLeft = Instantiate(newLeft,baseLeft.position,baseLeft.rotation)as GameObject;
 			baseLeft.renderer.enabled = false;
 			curLeft.transform.parent = baseLeft;
-		}
+		}else Debug.Log ("Left not found, reverting to default");
 		if(newRight!=null){
 			Debug.Log("Right found!");
 			curRight = Instantiate(newRight,baseRight.position,baseRight.rotation)as GameObject;
 			baseRight.renderer.enabled = false;
 			curRight.transform.parent = baseRight;
-		}
+		}else Debug.Log ("Right not found, reverting to default");
 	}
 	public void Deskin(){
 		GameObject podToKill = curPod;
@@ -51,5 +63,7 @@ public class PodSkin : MonoBehaviour {
 		basePod.renderer.enabled = true;
 		baseLeft.renderer.enabled = true;
 		baseRight.renderer.enabled = true;
+
+		Resources.UnloadUnusedAssets();
 	}
 }

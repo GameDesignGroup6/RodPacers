3d0655d6bef8b4b91fc6d9602c87f9fc81cc2632
yourgamesbeover <yourgamesbeover@gmail.com>
2014-04-20 20:57:56 -0400

    finally fixed health and added HurtZones!

--- Assets/Scripts/EngineHealth.cs
+++ Assets/Scripts/EngineHealth.cs
@@ -1,58 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Collider),typeof(Engine))]
+[RequireComponent(typeof(Collider))]
 public class EngineHealth : MonoBehaviour {
 	public float maxHealth = 1000f;
-	private Engine engine;
-	public Engine otherEngine;
-	private EnginePair enginePair;
-	private Transform pod;
-	private Transform trans;
 	private float curHealth;
 	public float Health{
 		get{return curHealth;}
 	}
-	public float angleThreshold = 30f;
-	private Transform left,right;
+	private float inv = 0f;
 
 	// Use this for initialization
-	void Start () {
-		engine = GetComponent<Engine>();
-		enginePair = engine.enginePair;
-		pod = engine.enginePair.transform;
-		trans = transform;
-		curHealth = maxHealth;
-		if(engine.Side==Engine.EngineSide.LEFT){
-			left = engine.EngineTransform;
-			right = otherEngine.EngineTransform;
-		}else{
-			left = otherEngine.EngineTransform;
-			right = engine.EngineTransform;
-		}
-
-	}
 	void OnCollisionStay(Collision col){
+		if(inv<0f)return;
+		inv = 0.25f;
 		Vector3 velocity = col.relativeVelocity;
 		Vector3 normal = col.contacts[0].normal;
-		float damage = Vector3.Dot(normal,velocity);
-		curHealth-=Mathf.Abs(damage);
+		float speed = Vector3.Project(velocity,normal).magnitude;
+		float damage = speed/2f;
+		Debug.Log ("HIT speed: "+speed+", damage: "+damage);
+		curHealth-=damage*damage;
 	}
-
 	void FixedUpdate(){
-		if(enginePair.EnginesSwitched)curHealth-=5f;
-
-		float angleDelta = Vector3.Angle(left.up,right.up);
-
-		if(angleDelta>angleThreshold)curHealth-=1;
-		//DebugHUD.setValue(name+" angleDelta",angleDelta);
-
-		Vector3 localPod = trans.InverseTransformPoint(pod.position);
-		if(localPod.y>0f){
-			curHealth-=5f;
-			//DebugHUD.setValue(name+": Pod in front","<color=red>TRUE</color>");
-		}else{
-			//DebugHUD.setValue(name+": Pod in front","<color=green>FALSE</color>");
+		if(inv>0.0f){
+			inv-=0.005f;
+			if(inv<0f)inv=0f;
+		}
+	}
+	public void Hurt(float damage){
+		curHealth-=damage;
+		if(curHealth<=0f){
+			Debug.Log ("DEAD!");
 		}
 	}
 }
---
+++ Assets/Scripts/HurtZone.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(Collider))]
+public class HurtZone : MonoBehaviour {
+	public float damage;
+	void OnTriggerStay(Collider other){
+		EngineHealth health = other.gameObject.GetComponent<EngineHealth>();
+		if(health==null)return;
+		health.Hurt(damage);
+	}
+}

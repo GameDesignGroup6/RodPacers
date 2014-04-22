7d79a84c7310dc3aa77287a79a65dea940ece746
abc33kr <abc33kr@gmail.com>
2014-04-21 23:14:33 -0400

    I made some changes in the NodeSystem while assuming the nodes will become bunch of finish lines.

---
+++ Assets/Scripts/NodeSystem/MovingAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingAI : PodScript {
+	private float speed;
+	// Use this for initialization
+	void Start () {
+		speed = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, goalNode.Position, speed*Time.deltaTime);
+	}
+
+	//Vector3 difference (){
+	//	return goalNode.Position - currentNode.Position;
+	//}
+}

2ec5c82e7a5dd45a7f9b655ec05c08c3d095c869
abc33kr <abc33kr@gmail.com>
2014-04-27 02:11:15 -0400

    updated podlabel
    ditched Vector3.distance
    font looks nicer and scales off distance.

--- Assets/Scripts/Map/PodLabel.cs
+++ Assets/Scripts/Map/PodLabel.cs
@@ -5,21 +5,25 @@ using System.Collections;
  *Attach this script to the cube inside the pod
  */
 public class PodLabel : MonoBehaviour {
-
-
+	
 	public int playerNum;
 	public GUIText label;
-
 
 	public Transform pod;
 	public float maxDist;
 
 	public Camera[] podCams;
+
+	public int initialFontSize = 14;
+	public float fontScaling = 1f;
 	// Use this for initialization
 	void Start () {
 		playerNum = int.Parse(transform.root.name.Substring(transform.root.name.Length-1));
 		pod = transform.parent;
 		label = transform.root.GetComponentInChildren<GUIText>();
+		label.fontSize = initialFontSize;
+		label.fontStyle = FontStyle.Bold;
+		label.color = Color.red;
 		maxDist = 100;
 		int i = 0;
 		foreach (Camera cam in Camera.allCameras){
@@ -33,18 +37,26 @@ public class PodLabel : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		for (int i = 0; i<podCams.Length; i++){
-
-			if (nearBy (podCams[i])){
+			if (nearby (podCams[i])){
 				label.text = "Player " + playerNum;
-				label.transform.position = podCams[i].WorldToViewportPoint(pod.transform.position + Vector3.up);
+				label.fontSize = (int)(initialFontSize * fontScaling);
+				label.transform.position = podCams[i].WorldToViewportPoint(pod.transform.position + 3*Vector3.up);
+				scaleFont (podCams[i]);
+				//Invoke("scaleFont", 1f);
 			}
 		}
 	}
 
-	bool nearBy (Camera podCam){
-		if (Vector3.Distance(podCam.transform.position, pod.transform.position) < maxDist)
+	bool nearby (Camera podCam){
+		//doing this since we don't have to be extremely precise with the max distance where the label is visible.
+		Vector3 difference = podCam.transform.position - pod.transform.position;
+		if (difference.x <maxDist && difference.z <maxDist)
 			return true;
 		else return false;
+	}
+	void scaleFont (Camera podCam){
+		Vector3 difference = podCam.transform.position - pod.transform.position;
+		fontScaling = 1f - (difference.sqrMagnitude / Mathf.Pow(1.41f * maxDist,2));
 	}
 
 	void OnWillRenderObject(){

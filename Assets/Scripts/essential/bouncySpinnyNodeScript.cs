6606c7bc931f4cc164f27d72c55906dd7922e7d0
las203 <las203@case.edu>
2014-04-29 14:05:10 -0400

    bouncyspinnynodescript

---
+++ Assets/Scripts/essential/bouncySpinnyNodeScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class bouncySpinnyNodeScript : MonoBehaviour {
+
+	Quaternion quart;
+
+	void Start () {
+		Node[] array = GetComponentsInChildren<Node>();
+		for (int i = 0; i < array.Length; i++) {
+			if (array[i].next != null) {
+				quart = Quaternion.LookRotation(array[i].next.transform.position - array[i].transform.position, Vector3.up);
+				array[i].transform.rotation = quart;
+			}
+		}
+	}
+}

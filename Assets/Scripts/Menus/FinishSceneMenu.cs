f0e3d6fb6bf9a1a5e6ae92677b55b5300b70e1dd
chhf129 <hxc334@case.edu>
2014-04-29 13:32:41 -0400

    more stuff

---
+++ Assets/Scripts/Menus/FinishSceneMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinishSceneMenu : MonoBehaviour {
+	string restartScene;
+	// Use this for initialization
+	void Start () {
+		restartScene = GetComponent<StartMenu> ().nextScenes [0];
+		if (PlayerManager.playerCount == 1)
+						restartScene = "SinglePlayer";
+		else if (PlayerManager.playerCount == 2)
+			restartScene = "TwoPlayer";
+		else if (PlayerManager.playerCount == 3)
+			restartScene = "ThreePlayer";
+		else if (PlayerManager.playerCount == 4)
+			restartScene = "FourPlayer";
+		else
+			restartScene = "SinglePlayer";
+		GetComponent<StartMenu> ().nextScenes [0] = restartScene;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

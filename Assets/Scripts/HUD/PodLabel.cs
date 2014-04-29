64fcc2f1363a33152f730d1705887ce319293693
abc33kr <abc33kr@gmail.com>
2014-04-29 15:06:59 -0400

    PodLabel update and adding guitext to pods

--- Assets/Scripts/HUD/PodLabel.cs
+++ Assets/Scripts/HUD/PodLabel.cs
@@ -7,29 +7,33 @@ using System.Collections;
 public class PodLabel : MonoBehaviour {
 	
 	public int playerNum;
-	public GUIText label;
 
 	public Transform pod;
 	public float maxDist;
 
 	public Camera[] podCams;
+	public GUIText[] podLabels;
 
 	public int initialFontSize = 14;
 	public float fontScaling = 1f;
 	// Use this for initialization
 	void Start () {
+		podCams = new Camera[4];
+		podLabels = new GUIText[4];
+		maxDist = 100;
 		playerNum = int.Parse(transform.root.name.Substring(transform.root.name.Length-1));
 		pod = transform.parent;
-		label = transform.root.GetComponentInChildren<GUIText>();
-		label.fontSize = initialFontSize;
-		label.fontStyle = FontStyle.Bold;
-		label.color = Color.red;
-		maxDist = 100;
-		int i = 0;
 		foreach (Camera cam in Camera.allCameras){
 			if ((cam.name.Substring(0,6) == "Camera")&&(cam.name.Substring(6) != ""+ playerNum)){
-				podCams[i] = cam;
-				i++;
+				podCams[int.Parse(cam.name.Substring (6)) -1] = cam;
+			}
+		}
+		foreach (GUIText label in transform.root.GetComponentsInChildren<GUIText>()){
+			if ((label.name.Substring (0, "Label".Length) == "Label")&&(label.name.Substring("Label".Length) != ""+ playerNum)){
+				label.fontSize = initialFontSize;
+				label.fontStyle = FontStyle.Bold;
+				label.color = Color.red;
+				podLabels[int.Parse (label.name.Substring("Label".Length))- 1] = label;
 			}
 		}
 	}
@@ -38,10 +42,11 @@ public class PodLabel : MonoBehaviour {
 	void Update () {
 		for (int i = 0; i<podCams.Length; i++){
 			if (nearby (podCams[i])){
-				label.text = "Player " + playerNum;
-				label.fontSize = (int)(initialFontSize * fontScaling);
-				label.transform.position = podCams[i].WorldToViewportPoint(pod.transform.position + 3*Vector3.up);
 				scaleFont (podCams[i]);
+				podLabels[i].text = "Player " + playerNum;
+				podLabels[i].fontSize = (int)(initialFontSize * fontScaling);
+				podLabels[i].transform.position = podCams[i].WorldToViewportPoint(pod.transform.position + 3*Vector3.up);
+
 				//Invoke("scaleFont", 1f);
 			}
 		}
@@ -50,21 +55,23 @@ public class PodLabel : MonoBehaviour {
 	bool nearby (Camera podCam){
 		//doing this since we don't have to be extremely precise with the max distance where the label is visible.
 		Vector3 difference = podCam.transform.position - pod.transform.position;
-		if (difference.x <maxDist && difference.z <maxDist)
+		if (Mathf.Abs (difference.x) <maxDist && Mathf.Abs (difference.z) <maxDist)
 			return true;
 		else return false;
 	}
+
 	void scaleFont (Camera podCam){
 		Vector3 difference = podCam.transform.position - pod.transform.position;
 		fontScaling = 1f - (difference.sqrMagnitude / Mathf.Pow(1.41f * maxDist,2));
 	}
 
+	//also realized we have no way of knowing which guitext to edit.
 	void OnWillRenderObject(){
 		Camera cam = Camera.current;
 		if (cam.name == "MiniMap" || cam.name == "SceneCamera")
 			return;
-		label.text = "Player " + playerNum;
-		label.transform.position = cam.WorldToViewportPoint(pod.transform.position + Vector3.up);
+		podLabels[0].text = "Player " + playerNum;
+		podLabels[0].transform.position = cam.WorldToViewportPoint(pod.transform.position + Vector3.up);
 		Debug.Log ("I am " + cam.name + "and I am viewing " + transform.name);
 
 	}

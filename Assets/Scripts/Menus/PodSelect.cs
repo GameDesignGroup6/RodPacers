1fd5afb83ef33a98594a61728bb913fd345908ea
freshvictory <jsren1@verizon.net>
2014-04-30 15:45:07 -0400

    better selection screens

--- Assets/Scripts/Menus/PodSelect.cs
+++ Assets/Scripts/Menus/PodSelect.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 [RequireComponent(typeof(PodSkin),typeof(bouncySpinnyCubeScript))]
 public class PodSelect : MonoBehaviour {
+	public GUIText podDescription;
 	private PodSkin skinToChange;
 	private int selectedPod;
 	public int playerNumber;
@@ -31,19 +32,36 @@ public class PodSelect : MonoBehaviour {
 			updateIsTooFast-=Time.deltaTime;
 			return;
 		}
-		if(Input.GetAxis("LeftStickHoriz"+playerNumber)<-0.025f){
+		if(Input.GetAxis("LeftStickHoriz"+playerNumber)<-0.7f){
 			selectedPod--;
 			if(selectedPod<minPodNumber&&!(Input.GetKey("joystick "+playerNumber+" button 6")&&selectedPod>=0))selectedPod = maxPodNumber;
 			skinToChange.ChangeSkin(selectedPod);
 			updateIsTooFast = 0.25f;
 			PlayerManager.podSkins[playerNumber-1] = selectedPod;
 		}
-		if(Input.GetAxis("LeftStickHoriz"+playerNumber)>0.025f){
+		if(Input.GetAxis("LeftStickHoriz"+playerNumber)>0.7f){
 			selectedPod++;
 			if(selectedPod>maxPodNumber && !(Input.GetKey("joystick "+playerNumber+" button 6")&&selectedPod<SkinManager.SkinList.Length))selectedPod = minPodNumber;
 			skinToChange.ChangeSkin(selectedPod);
 			updateIsTooFast = 0.25f;
 			PlayerManager.podSkins[playerNumber-1] = selectedPod;
+		}
+
+		switch(selectedPod) {
+		case 0: podDescription.text = "Default";
+			break;
+		case 1: podDescription.text = "Custom Radon-Ulzer 620C";
+			break;
+		case 2: podDescription.text = "Plug-F Mammoth";
+			break;
+		case 3: podDescription.text = "KVT9T9-B Wasp";
+			break;
+		case 4: podDescription.text = "GPE-3130";
+			break;
+		case 5: podDescription.text = "Twin Telephone Booths";
+			break;
+		default: podDescription.text = "Pod";
+			break;
 		}
 	}
 }

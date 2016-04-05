4ac1bacf531b25b8763e40c871a28c588226bf96
Steven <yourgamesbeover@gmail.com>
2016-04-04 23:02:06 -0400

    menu?

--- Assets/Scripts/Menus/StartMenu.cs
+++ Assets/Scripts/Menus/StartMenu.cs
@@ -3,62 +3,80 @@ using System.Collections;
 
 public class StartMenu : MonoBehaviour {
 
-	// The options to choose from
-	public GUIText[] menuChoices;
+    // The options to choose from
+    public GUIText[] menuChoices;
 
-	// Where each option leads
-	public string[] nextScenes;
+    // Where each option leads
+    public string[] nextScenes;
 
-	// The current position in the menu
-	private int position = 0;
+    // The current position in the menu
+    private int position = 0;
 
-	// Make it so it only changes every quarter second if you hold down the button
-	private float updateIsTooFast = .25f;
+    // Make it so it only changes every quarter second if you hold down the button
+    private float updateIsTooFast = .25f;
 
-	void Start () {
-		Cursor.visible = false;
-		Screen.sleepTimeout = SleepTimeout.NeverSleep;
-		menuChoices[0].color = Color.white;
-	}
+    void Start() {
+        Cursor.visible = false;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        //menuChoices[0].color = Color.white;
+    }
 
-	// Update is called once per frame
-	void Update () {
-		if (updateIsTooFast > 0) {
-			updateIsTooFast = updateIsTooFast - Time.deltaTime;
-			return;
-		}
+    // Update is called once per frame
+    void Update() {
+        //begin IOS changes
+        if (Input.touchCount == 1) {
+            var touch = Input.touches[0];
+            if (touch.phase == TouchPhase.Ended) {
+                for (int x = 0; x < menuChoices.Length; x++) {
+                    if (menuChoices[x].HitTest(touch.position)) {
+                        if (nextScenes[x] == "Quit") {
+                            Application.Quit();
+                        } else {
+                            Application.LoadLevel(nextScenes[x]);
+                        }
+                    }
+                }
+            }
+        }
+        //end IOS changes
 
-		if (Input.GetAxis ("LeftStickVert1") > 0.1f) {
-			menuChoices[position].color = Color.yellow;
-			position--;
-			if (position < 0) {
-				position = menuChoices.Length - 1;
-			}
-			updateIsTooFast = 0.25f;
-		}
-		if (Input.GetAxis ("LeftStickVert1") < -0.1f) {
-			menuChoices[position].color = Color.yellow;
-			position = (position + 1)%menuChoices.Length;
-			updateIsTooFast = 0.25f;
-		}
+        if (updateIsTooFast > 0) {
+            updateIsTooFast = updateIsTooFast - Time.deltaTime;
+            return;
+        }
 
-		menuChoices[position].color = Color.white;
+        if (Input.GetAxis("LeftStickVert1") > 0.1f) {
+            menuChoices[position].color = Color.yellow;
+            position--;
+            if (position < 0) {
+                position = menuChoices.Length - 1;
+            }
+            updateIsTooFast = 0.25f;
+        }
+        if (Input.GetAxis("LeftStickVert1") < -0.1f) {
+            menuChoices[position].color = Color.yellow;
+            position = (position + 1) % menuChoices.Length;
+            updateIsTooFast = 0.25f;
+        }
 
-		if (Input.GetAxis ("Throttle1") > 0) {
-			if (nextScenes[position] == "Quit") {
-				Application.Quit ();
-			}
-			else {
-				Application.LoadLevel (nextScenes[position]);
-			}
-		}
+        menuChoices[position].color = Color.white;
 
-		// B makes it go back but not quit
-		if (Input.GetAxis ("Throttle1") < 0) {
-			if (nextScenes[nextScenes.Length - 1] != "Quit") {
-				Application.LoadLevel (nextScenes[nextScenes.Length - 1]);
-			}
-		}
-	}
-	
+        if (Input.GetAxis("Throttle1") > 0) {
+            if (nextScenes[position] == "Quit") {
+                Application.Quit();
+            } else {
+                Application.LoadLevel(nextScenes[position]);
+            }
+        }
+
+        // B makes it go back but not quit
+        if (Input.GetAxis("Throttle1") < 0) {
+            if (nextScenes[nextScenes.Length - 1] != "Quit") {
+                Application.LoadLevel(nextScenes[nextScenes.Length - 1]);
+            }
+        }
+
+
+    }
+
 }
